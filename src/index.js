75a089906d5a0682f2b41dfff6cc1eb812d44c4d
Abhinav <abhinavsyunary@gmail.com>
2026-03-16 15:35:46 +0530

    matches added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchesRouter } from './routes/matches.js';
 const app = express();
 const port = 8000;
 
@@ -8,6 +9,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 }
 );
+
+app.use('/matches', matchesRouter);
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 }
---
+++ src/routes/matches.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+import { createMatchSchema, listMatchesQuerySchema } from "../validation/matches.js";
+import { db } from "../db/db.js";
+import { matches } from "../db/schema.js";
+export const matchesRouter = Router();
+import { getMatchStatus } from "../utils/match-status.js";
+import { desc } from "drizzle-orm";
+
+matchesRouter.get("/", async(req, res) => {
+  const parsed=listMatchesQuerySchema.safeParse(req.query);
+  if (!parsed.success) {
+    return res.status(400).json({ error:'invalid query parameters', details: parsed.error.errors });
+  }
+  const limit=Math.min(parsed.data.limit ?? 50, 100);
+
+try {    
+    const data = await db.select().from(matches).orderBy((desc(matches.createdAt))).limit(limit);
+    res.json({ data: data });
+  } catch (error) { 
+    res.status(500).json({ error: 'Internal server error',error: error.message });  
+  }
+     
+});
+
+matchesRouter.post("/", async(req, res) => {
+  const parsed= createMatchSchema.safeParse(req.body);
+  if (!parsed.success) {
+    return res.status(400).json({ error:'invalid input', details: parsed.error.errors });
+  } 
+  try {
+    const [event]=await db.insert(matches).values({
+      ...parsed.data,
+      startTime: new Date(parsed.data.startTime),
+      endTime: new Date(parsed.data.endTime),
+      homeScore: parsed.data.homeScore ?? 0,
+      awayScore: parsed.data.awayScore ?? 0,
+      status:getMatchStatus(parsed.data.startTime, parsed.data.endTime),
+    }).returning();
+    res.status(201).json({ message: "Match created",data: event });
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Internal server error',error: error.message });
+  }
+});
